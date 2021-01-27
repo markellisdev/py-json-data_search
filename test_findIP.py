a19aed887d93fab30556827372ed0a93e5fab72e
Mark Ellis <markellisdev@gmail.com>
2021-01-27 11:12:37 -0600

    added mock test for testing getResponse function

--- test_findIP.py
+++ test_findIP.py
@@ -1,17 +1,24 @@
-import unittest
+# Third-party imports...
+from nose.tools import assert_true, assert_is_not_none
+import unittest, requests
+
+# Standard library imports...
+from unittest.mock import Mock, patch
+
+#Local imports
 from findIP import IPAddress
 
 
-class TestFunc(unittest.TestCase):
-    """
-    Test Case for User Input function
-    """
-    def test_user_input_is_not_null(self):
-        # self.assertIsNotNone(findIP.userInput)
-        pass
+@patch('findIP.IPAddress.getResponse')
+def test_getting_API_response(mock_get):
+    # Configure the mock to return a response with an OK status code.
+    mock_get.status_code = 200
 
-    def getresponse_returns_valid_json(self):
-        self.assertIsNotNone(IPAddress.getResponse(IPAddress.url))
+    # Call the service, which will send a request to the server.
+    operUrl = IPAddress.getResponse()
+
+    # If the request is sent successfully, then I expect a response to be returned.
+    assert_is_not_none(operUrl)
 
 
 if __name__ == "__main__":
