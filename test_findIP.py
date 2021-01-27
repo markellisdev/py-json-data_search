ee61c1e5e6d1e71ae306f2a647b8e666adb0b44a
Mark Ellis <markellisdev@gmail.com>
2021-01-27 11:40:52 -0600

    test getting response, but still actually calling API mock not working because never calls function

--- test_findIP.py
+++ test_findIP.py
@@ -9,6 +9,20 @@ from unittest.mock import Mock, patch
 from findIP import IPAddress
 
 
+"""
+Initial test to see that actual response is returned
+"""
+def test_request_response():
+    # Send a request to the API server and store the response.
+    response = IPAddress.getResponse(IPAddress.url)
+
+    # If the request is sent successfully, then I expect a response to be returned.
+    assert_is_not_none(response)
+
+"""
+Not yet working mock because need access requests.get direct
+"""
+
 @patch('findIP.IPAddress.getResponse')
 def test_getting_API_response(mock_get):
     # Configure the mock to return a response with an OK status code.
